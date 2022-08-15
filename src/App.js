720e64163af31480f20ab4aba237ac479c54b477
nadimahmed1 <nadimarafat26@gmail.com>
2022-08-15 18:31:53 +0600

    useState commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products = [
-    { name: 'Hp laptop', price: 150000 },
-    { name: 'Iphone', price: 50000 },
-    { name: 'Watch', price: 1500 },
-    { name: 'Breslate', price: 1800 },
-  ]
 
   return (
     <div className="App">
+      <Counter></Counter>
 
-      {
-        products.map(product => <Product name={product.name} price={product.price}></Product>)
-      }
-
-      {/* <Product name="Laptop" price="80000"></Product>
-      <Product name="Phone" price="75000"></Product>
-      <Product name="Watch" price="750"></Product> */}
     </div>
   );
 }
 
-function Product(props) {
+function Counter() {
+  const [count, setCount] = useState(85)
   return (
-    <div className='product'>
-      <h3>Name: {props.name}</h3>
-      <p>Price: {props.price}</p>
+    <div>
+      <h1>Count: {count}</h1>
+      <button>Increase</button>
     </div>
   )
 }
 
 export default App;
+
