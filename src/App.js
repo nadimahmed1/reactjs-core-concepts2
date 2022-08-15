672ec7720d226dcde8497a0f119f7591b95791d1
nadimahmed1 <nadimarafat26@gmail.com>
2022-08-15 19:11:34 +0600

    users API load

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
 
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExtarnalUsers></ExtarnalUsers>
 
     </div>
   );
 }
 
+function ExtarnalUsers() {
+  const [users, setUsers] = useState([]);
+  // useEffect(()=>{} , []);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
+  return (
+    <div>
+      <h1>Extarnal Users</h1>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  return (
+    <div style={{ border: '2px solid red', margin: '20px' }}>
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
+    </div>
+  )
+}
+
+// 
 function Counter() {
   const [count, setCount] = useState(85);
   // one line work
